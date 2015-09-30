e041c6e247ec3dd9422a81e703a95f770253537c
cvrlebg <milosh.mar@gmail.com>
2015-09-30 17:27:56 +0200

    init plugin

--- libs/kaltura/kaltura.sources.js
+++ libs/kaltura/kaltura.sources.js
@@ -1,42 +1,48 @@
 
 var kalturaSrc = {
-  getSource: function(selector, partnerId) {
-    $('.' + selector).each(function (k, playerObj) {
+  partnerId : '',
 
-      var obj = $(this);
-      var player = obj.find('video');
-      var entryId = player.attr('data-entryid');
+  getSrcForEachElement: function(elClass) {
+    var ks = this;
+    $(elClass).each(function (k, playerObj) {
+      ks.getSource(playerObj);
+    });
+  },
 
-      kWidget.getSources({
-        'partnerId': partnerId,
-        'entryId': entryId,
-        'callback': function (data) {
+  getSource: function(el) {
+    var obj = $(el);
+    var player = obj.find('video');
+    var entryId = player.attr('data-entryid');
 
-          $(document).ready(function () {
-            var videoData = data.sources;
-            var posterImg = data.poster + '/width/800';
+    kWidget.getSources({
+      'partnerId': this.partnerId,
+      'entryId': entryId,
+      'callback': function (data) {
 
-            var poster = obj.children('.vjs-poster').css({
-              'display': 'block',
-              'background-image': 'url(' + posterImg + ')'
-            });
+        $(document).ready(function () {
+          var videoData = data.sources;
+          var posterImg = data.poster + '/width/800';
 
-            //set attr
-            player.attr('title', data.name);
-            $('.video_title').html(data.name);
-
-            videoData.forEach(function (k, v) {
-              if (k.type === 'video/h264' && (/(a.mp4)/).test(k.src) && !k.isOriginal) {
-                var src = $('<source />');
-                src.attr('src', k.src);
-                src.attr('type', k.type.replace('video/h264', 'video/mp4'));
-                player.append(src);
-              }
-            });
-
+          var poster = obj.children('.vjs-poster').css({
+            'display': 'block',
+            'background-image': 'url(' + posterImg + ')'
           });
-        }
-      });
+
+          //set attr
+          player.attr('title', data.name);
+          $('.video_title').html(data.name);
+
+          videoData.forEach(function (k, v) {
+            if (k.type === 'video/h264' && (/(a.mp4)/).test(k.src) && !k.isOriginal) {
+              var src = $('<source />');
+              src.attr('src', k.src);
+              src.attr('type', k.type.replace('video/h264', 'video/mp4'));
+              player.append(src);
+            }
+          });
+
+        });
+      }
     });
   }
 }
